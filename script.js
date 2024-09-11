169ff975ac9a401b11710cf7e55d1b7bff4650f5
Evgeniy Buslovich <evgeniy_buslovich@epam.com>
2024-09-11 18:34:26 +0200

    forgot to save so pushed just emtpy files, now it is fixed. HW_2_1_2

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstName = "Evgeniy"; //here we put string in variable
+console.log(firstName);
+{
+let firstName = {
+    "age": 38,
+    "firstName": "Evgeniy",
+    "lastName": "Buslovich"
+}
+console.log(firstName);
+}
