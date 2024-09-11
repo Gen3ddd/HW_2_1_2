a4fa4916118d36f3311e64eebae7902f89ce4dd6
Evgeniy Buslovich <evgeniy_buslovich@epam.com>
2024-09-11 21:17:06 +0200

    saved the file and now push with content

---
+++ task.score.js
@@ -0,0 +1,10 @@
+let intro = 8;
+let git = 9;
+let js = 8;
+let averageScore = (intro + git + js) / 3;
+let firstName = "Sergey";
+let surName = "Ivanov";
+
+console.log(
+  `Average score of a student ${firstName} ${surName} is: ${averageScore}`
+);
